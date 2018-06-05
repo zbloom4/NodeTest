086e90a868c12fc0a9fdb6e6c7a700aec5e3f2ff
Zachary Bloom <zacharybloom@Zacharys-MBP.fios-router.home>
2018-06-05 11:56:41 -0700

    Add cool face API

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cool = require('cool-ascii-faces')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -7,4 +8,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
